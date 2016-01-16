200ab36cef3f274b5d7103204506759278ef031f
MikhailSorokin <sorokinmike@yahoo.com>
2016-01-16 15:03:22 -0500

    Uploading my first DayOne challenge to GitHub.

---
+++ DayOne.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        System.out.println("Primitive : double"); //5.25
+        System.out.println("Primitive : char"); //'a'
+        System.out.println("Primitive : boolean"); //false
+        System.out.println("Primitive : int"); //100
+        System.out.println("Referenced : String"); //"I am a code monkey."
+        System.out.println("Primitive : boolean"); //true
+        System.out.println("Primitive : double"); //17.3
+        System.out.println("Primitive : char"); //'c'
+        System.out.println("Referenced : String"); //"derp"
+    }
+}
